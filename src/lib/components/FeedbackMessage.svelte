de99186a7fff123c4a7741cba8e46e7ef706ab40
simicjasna <simicjas@students.zhaw.ch>
2026-05-06 17:43:06 +0200

    feat(components): add feedbackmessage component

---
+++ src/lib/components/FeedbackMessage.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { feedback } = $props();
+</script>
+
+{#if feedback}
+	<div
+		class:success-message={feedback.type === 'success'}
+		class:danger-message={feedback.type === 'danger'}
+		class:warning-message={feedback.type === 'warning'}
+	>
+		{feedback.message}
+	</div>
+{/if}
